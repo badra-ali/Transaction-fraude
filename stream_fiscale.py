90382a7d54dca81feede20ceb79a086da57d4ded
Alidjou BAMBA <80393136+badra-ali@users.noreply.github.com>
2024-03-02 16:47:20 +0000

    Update stream_fiscale.py

--- stream_fiscale.py
+++ stream_fiscale.py
@@ -1,13 +1,5 @@
 import streamlit as st
 st.write("BAMBA")
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.image('Marche-Financier.png',caption='Les Valeurs du Marché')
-with col2:
-    st.image('Marche-Financier.png',caption='Les Valeurs du Marché')
-    
-with col3:
-    st.image('Marche-Financier.png',caption='Les Valeurs du Marché')
 
 
 

4114cd78d179c2c625691253349e8c96f985ea00
Alidjou BAMBA <80393136+badra-ali@users.noreply.github.com>
2024-03-04 10:07:36 +0000

    Update stream_fiscale.py

--- stream_fiscale.py
+++ stream_fiscale.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import requests
+import requests
 #from bs4 import BeautifulSoup
 #import schedule
 #import matplotlib.pyplot as plt

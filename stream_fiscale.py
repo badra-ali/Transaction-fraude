48baf0753259a8debc040ff20dd1104014d4d567
Alidjou BAMBA <80393136+badra-ali@users.noreply.github.com>
2024-03-05 08:58:08 +0000

    Update stream_fiscale.py

--- stream_fiscale.py
+++ stream_fiscale.py
@@ -3,12 +3,12 @@ import pandas as pd
 import numpy as np
 import requests
 from bs4 import BeautifulSoup
-#import schedule
-#import matplotlib.pyplot as plt
-#import seaborn as sns
-#from sklearn.model_selection import train_test_split
-#from sklearn.linear_model import LinearRegression
-#from sklearn.metrics import mean_squared_error
+import schedule
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
 # Charger les donnéesst.cache_data
 """@st.cache_data
